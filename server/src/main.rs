455c96efea2e5305b90b4615f4fd7f8c079b3863
Kim Seungha <seungha.me@gmail.com>
2021-08-09 22:08:17 +0900

    fix: binding ip

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,7 +14,7 @@ async fn main() -> std::io::Result<()> {
             .data(srv_tx.clone())
             .configure(root)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
